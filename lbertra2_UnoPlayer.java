2d1d6cbbfd4654f056ed1101dda561d1480a208a
jacob tom <tjacob3@912e006-01.iut-bm.univ-fcomte.fr>
2018-11-26 13:58:39 +0100

    Ajout des fichiers essentiels au projet

---
+++ lbertra2_UnoPlayer.java
@@ -0,0 +1,24 @@
+
+package uno;
+
+import java.util.List;
+
+public class lbertra2_UnoPlayer implements UnoPlayer {
+
+
+    public int play(List<Card> hand, Card upCard, Color calledColor,
+        GameState state) {
+
+        // Votre code ici
+        return -1;
+    }
+
+
+
+    public Color callColor(List<Card> hand) {
+
+        // Votre code ici
+        return null;
+    }
+
+}
